10321960a2921ffffa5ca99924025a527d6de61b
Abdelrhman Fikri <108499713+abdelrhman2148@users.noreply.github.com>
2024-11-15 13:53:51 +0200

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,11 +13,16 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+
 @app.errorhandler(401)
 def unauthorized(error):
     """Handle unauthorized errors."""
     return jsonify({"error": "Unauthorized"}), 401
 
+@app.errorhandler(403)
+def forbidden(error):
+    """Handle forbidden errors."""
+    return jsonify({"error": "Forbidden"}), 403
 
 @app.errorhandler(404)
 def not_found(error) -> str:
