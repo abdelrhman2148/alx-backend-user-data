4c758141914399340017518f888848a507f8e74e
Abdelrhman Fikri <108499713+abdelrhman2148@users.noreply.github.com>
2024-11-15 13:46:54 +0200

    Update index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -4,6 +4,11 @@
 from flask import jsonify, abort
 from api.v1.views import app_views
 
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized_route():
+    """Route that triggers a 401 error."""
+    abort(401)
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
